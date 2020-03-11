44263a7c7cad739cd92e743cc2074396678451f4
Itabaiyu <itabaiyu@gmail.com>
2020-03-10 22:40:13 -0400

    Implement object pool

---
+++ src/Atrea.Utilities.Tests/Pool/ObjectPoolTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading;
+using Atrea.Utilities.Pool;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Atrea.Utilities.Tests.Pool
+{
+    [TestFixture]
+    public class ObjectPoolTests
+    {
+        private class Foo
+        {
+            public int Bar { get; set; }
+        }
+
+        [Test]
+        public void Get_Creates_New_Object_When_Pool_Is_Empty()
+        {
+            var objectPool = new ObjectPool<Foo>(() => new Foo());
+
+            var result = objectPool.Get();
+
+            result.Should().BeOfType<Foo>();
+        }
+
+        [Test]
+        public void Get_Reuses_Object_In_The_Pool()
+        {
+            var objectPool = new ObjectPool<Foo>(() => new Foo());
+
+            var foo = new Foo {Bar = 10};
+
+            objectPool.Put(foo);
+
+            var reusedFoo = objectPool.Get();
+
+            reusedFoo.Bar.Should().Be(foo.Bar);
+
+            var newFoo = objectPool.Get();
+
+            newFoo.Should().NotBeEquivalentTo(reusedFoo);
+        }
+    }
+}
---
+++ src/Atrea.Utilities/Pool/IObjectPool.cs
@@ -0,0 +1,21 @@
+﻿namespace Atrea.Utilities.Pool
+{
+    /// <summary>
+    ///     An object pool which efficiently handles retrieving and adding objects to the pool.
+    /// </summary>
+    /// <typeparam name="T">The type of object to use in the object pool.</typeparam>
+    public interface IObjectPool<T>
+    {
+        /// <summary>
+        ///     Retrieve an object from the pool, or create a new one if the pool is empty.
+        /// </summary>
+        /// <returns>Returns the object from the pool.</returns>
+        T Get();
+
+        /// <summary>
+        ///     Put an object back in the pool.
+        /// </summary>
+        /// <param name="item">The item to put back in the pool.</param>
+        void Put(T item);
+    }
+}
