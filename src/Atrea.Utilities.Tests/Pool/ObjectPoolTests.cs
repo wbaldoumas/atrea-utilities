292e24a91897f0610eae708e05870d3f159d08eb
Itabaiyu <45316999+itabaiyu@users.noreply.github.com>
2020-03-10 22:47:18 -0400

    Implement Object Pool (#4)

---
+++ src/Atrea.Utilities.Tests/Pool/ObjectPoolTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Atrea.Utilities.Pool;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Atrea.Utilities.Tests.Pool
+{
+    [TestFixture]
+    public class ObjectPoolTests
+    {
+        private class Foo
+        {
+            public int Bar { get; set; }
+        }
+
+        [Test]
+        public void Get_Creates_New_Object_When_Pool_Is_Empty()
+        {
+            var objectPool = new ObjectPool<Foo>(() => new Foo());
+
+            var result = objectPool.Get();
+
+            result.Should().BeOfType<Foo>();
+        }
+
+        [Test]
+        public void Get_Reuses_Object_In_The_Pool()
+        {
+            var objectPool = new ObjectPool<Foo>(() => new Foo());
+
+            var foo = new Foo {Bar = 10};
+
+            objectPool.Put(foo);
+
+            var reusedFoo = objectPool.Get();
+
+            reusedFoo.Bar.Should().Be(foo.Bar);
+
+            var newFoo = objectPool.Get();
+
+            newFoo.Should().NotBeEquivalentTo(reusedFoo);
+        }
+
+        [Test]
+        public void Null_Object_Generator_Function_Results_In_ArgumentNullException()
+        {
+            Func<ObjectPool<Foo>> act = () =>
+            {
+                var objectPool = new ObjectPool<Foo>(null);
+
+                return objectPool;
+            };
+
+            act.Should().Throw<ArgumentNullException>();
+        }
+    }
+}
---
+++ src/Atrea.Utilities/Pool/ObjectPool.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Atrea.Utilities.Pool
+{
+    /// <inheritdoc cref="IObjectPool{T}"/>
+    public class ObjectPool<T> : IObjectPool<T>
+    {
+        private readonly ConcurrentBag<T> _objects;
+        private readonly Func<T> _objectGenerator;
+
+        /// <summary>
+        ///     An object pool which efficiently handles retrieving and adding objects to the pool.
+        /// </summary>
+        /// <param name="objectGenerator">The object generator for generating new objects when the pool is empty.</param>
+        public ObjectPool(Func<T> objectGenerator)
+        {
+            _objects = new ConcurrentBag<T>();
+            _objectGenerator = objectGenerator ?? throw new ArgumentNullException(nameof(objectGenerator));
+        }
+
+        /// <inheritdoc cref="IObjectPool{T}.Get()"/>
+        public T Get()
+        {
+            return _objects.TryTake(out var item) ? item : _objectGenerator();
+        }
+
+        /// <inheritdoc cref="IObjectPool{T}.Put(T)"/>
+        public void Put(T item)
+        {
+            _objects.Add(item);
+        }
+    }
+}
